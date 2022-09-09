b43d1953f452889978c9ac6be14e6278fbca1949
ardaberrun <ardaberrun@gmail.com>
2022-09-09 22:19:33 +0300

    update handleEdit function and change styles

--- src/components/Todo/AddTodo.tsx
+++ src/components/Todo/AddTodo.tsx
@@ -25,53 +25,47 @@ function AddTodo() {
     }
   };
 
-  if (loading) {
-    return (
-      <>
-        <div className="flex items-center justify-center">
+  return (
+    <>
+      {loading ? (
+        <div className="flex items-center justify-center py-2.5">
           <Loading />
         </div>
-        <hr />
-      </>
-    );
-  }
-
-  return toggleAddButton ? (
-    <>
-      <div className="flex justify-center sm:justify-start items-center gap-2">
-        <button
-          onClick={() => {
-            setToggleAddButton(false);
-            setContent('');
-          }}
-          className="w-6 h-6 text-red-500 text-lg font-bold"
-        >
-          X
-        </button>
-        <Input
-          buttonClick={addTodo}
-          buttonText="Add"
-          inputChange={(e) => setContent(e.target.value)}
-          inputValue={content}
-          placeholder="New Todo"
-          buttonDisabled={content.length < 3}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter' && content.length >= 3) {
-              addTodo();
-            }
-          }}
-        />
-      </div>
-      <hr />
-    </>
-  ) : (
-    <>
-      <button
-        onClick={() => setToggleAddButton(true)}
-        className="w-max px-4 py-2 mx-auto sm:mx-0 rounded-md bg-green-500 text-white text-xl font-medium border-0 outline-0 focus:ring-green-500 focus:ring-offset-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2"
-      >
-        Add Todo
-      </button>
+      ) : toggleAddButton ? (
+        <div className="flex justify-center sm:justify-start items-center gap-2">
+          <button
+            onClick={() => {
+              setToggleAddButton(false);
+              setContent('');
+            }}
+            className="w-6 h-6 text-red-500 text-lg font-bold"
+          >
+            X
+          </button>
+          <Input
+            buttonClick={addTodo}
+            buttonText="Add"
+            inputChange={(e) => setContent(e.target.value)}
+            inputValue={content}
+            placeholder="New Todo"
+            buttonDisabled={content.length < 3}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter' && content.length >= 3) {
+                addTodo();
+              }
+            }}
+          />
+        </div>
+      ) : (
+        <div className="inline-flex justify-self-start">
+          <button
+            onClick={() => setToggleAddButton(true)}
+            className="w-max px-4 py-2 mx-auto sm:mx-0 rounded-md bg-green-500 text-white text-xl font-medium border-0 outline-0 hover:bg-green-600"
+          >
+            Add Todo
+          </button>
+        </div>
+      )}
       <hr />
     </>
   );
--- src/components/Todo/TodoItem.tsx
+++ src/components/Todo/TodoItem.tsx
@@ -10,28 +10,22 @@ type TodoItemProps = {
 function TodoItem({ todo }: TodoItemProps) {
   const { dispatch } = useGlobalContext();
   const [content, setContent] = useState<string>(todo.content || '');
-  const [toggleEdit, setToggleEdit] = useState(false);
+  const [editMode, setEditMode] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const handleToggle = async () => {
-    try {
-      const updatedTodo = { ...todo, isCompleted: !todo.isCompleted };
-      dispatch({ type: 'UPDATE_TODO', payload: { todo: updatedTodo } });
-
-      await updateTodo(updatedTodo);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  const handleEdit = async () => {
+  const handleEdit = async (action: string = 'content') => {
+    let editTodo: Todo;
     try {
       setLoading(true);
-      const updatedTodo = { ...todo, content };
-      dispatch({ type: 'UPDATE_TODO', payload: { todo: updatedTodo } });
 
-      await updateTodo(updatedTodo);
-      setToggleEdit(false);
+      if (action === 'toggle') {
+        editTodo = { ...todo, isCompleted: !todo.isCompleted };
+      } else {
+        editTodo = { ...todo, content };
+      }
+
+      const updatedTodo = await updateTodo(editTodo);
+      dispatch({ type: 'UPDATE_TODO', payload: { todo: updatedTodo } });
     } catch (e) {
       console.log(e);
     } finally {
@@ -41,29 +35,32 @@ function TodoItem({ todo }: TodoItemProps) {
 
   const handleDelete = async () => {
     try {
-      dispatch({ type: 'DELETE_TODO', payload: todo.id });
-      await deleteTodo(todo.id);
+      setLoading(true);
+
+      const deletedTodo = await deleteTodo(todo.id);
+      dispatch({ type: 'DELETE_TODO', payload: deletedTodo.id });
     } catch (e) {
       console.log(e);
+    } finally {
+      setLoading(false);
     }
   };
 
-  return loading ? (
-    <div className="bg-gray-200 p-2 rounded-lg flex justify-center items-center border">
-      <Loading />
-    </div>
-  ) : toggleEdit ? (
+  return editMode ? (
     <div className="flex gap-2 items-center">
       <button
         onClick={() => {
-          setToggleEdit(false);
+          setEditMode(false);
         }}
         className="w-6 h-6 text-red-500 text-lg font-bold"
       >
         X
       </button>
       <Input
-        buttonClick={handleEdit}
+        buttonClick={() => {
+          handleEdit();
+          setEditMode(false);
+        }}
         buttonDisabled={content.length < 3}
         buttonText="Edit"
         inputChange={(e) => setContent(e.target.value)}
@@ -72,43 +69,56 @@ function TodoItem({ todo }: TodoItemProps) {
         onKeyDown={(e) => {
           if (e.key === 'Enter' && content.length >= 3) {
             handleEdit();
+            setEditMode(false);
           }
         }}
       />
     </div>
   ) : (
     <li
-      onClick={handleToggle}
-      className={`p-2 rounded-lg flex flex-col gap-2 sm:flex-row sm:gap-4 justify-between items-center border cursor-pointer transition duration-500 ${
+      onClick={() => handleEdit('toggle')}
+      className={`relative p-2 rounded-lg flex flex-col gap-2 sm:flex-row sm:gap-4 justify-between items-center border cursor-pointer transition duration-500 ${
         todo.isCompleted ? 'bg-purple-600' : 'bg-gray-200'
       }`}
     >
+      {loading && (
+        <div
+          className={`${
+            todo.isCompleted ? 'bg-gray-600' : 'bg-purple-400'
+          } absolute p-2 rounded-lg w-full h-full opacity-40 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2`}
+        />
+      )}
       <span
-        className={`w-full flex-grow text-base font-normal overflow-clip ${
+        className={`relative w-full flex-grow text-base font-normal overflow-hidden whitespace-nowrap overflow-ellipsis break-words ${
           todo.isCompleted && 'text-white line-through'
         }`}
       >
+        {loading && (
+          <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
+            <Loading />
+          </div>
+        )}
         {todo.content}
       </span>
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-2 z-10">
         <button
           onClick={(e) => {
             e.stopPropagation();
 
             setContent(todo.content);
-            setToggleEdit(true);
+            setEditMode(true);
           }}
-          className="bg-yellow-400 text-white font-semibold py-1 px-4 rounded focus:ring-yellow-400 focus:ring-offset-gray-100 transition ease-in duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2"
+          className="bg-yellow-400 text-white font-semibold py-1 px-4 rounded"
         >
           Edit
         </button>
         <button
           onClick={(e) => {
             e.stopPropagation();
-            
+
             handleDelete();
           }}
-          className="bg-red-500 text-white font-semibold py-1 px-4 rounded focus:ring-red-500 focus:ring-offset-gray-100 transition ease-in duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2"
+          className="bg-red-500 text-white font-semibold py-1 px-4 rounded focus:ring-red-500 focus:ring-offset-inherit transition ease-in duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2"
         >
           Delete
         </button>
